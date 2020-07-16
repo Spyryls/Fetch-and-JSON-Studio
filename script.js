1cc4a93011de0aadc88d1b2170a76fdd61d58907
Jason VanBlarcum <yr410ad@gmail.com>
2020-07-15 21:14:26 -0500

    Completed Exercise

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
   function init() {
+    let container = document.getElementById('container');
       fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
     
         response.json().then(function (json) {
-          let container = document.getElementById('#container');
+
           console.log(json);
 
-          for (let i; i < json.length; i++) {
+          for (i in json) {
+            let activeColor = "red";
+            if (json[i].active) {
+              activeColor = "green"
+            }
+
             container.innerHTML += `
               <div class="astronaut">
                 <div class="bio">
                   <h3>${json[i].firstName} ${json[i].lastName}</h3>
                 <ul>   
                   <li>Hours in space: ${json[i].hoursInSpace}</li>
-                  <li>Active: ${json[i].active}</li>
+                  <li style= "color:${activeColor}">Active: ${json[i].active}</li>
                   <li>Skills: ${json[i].skills.join(", ")}</li>
                 </ul>
-                  <img class="avatar" src="images/${json[i].picture}"/>
-                </div>
+                </div>                
+                  <img class="avatar" src="${json[i].picture}"/>
               </div>
             `;
           }
